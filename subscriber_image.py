7f46c27ecab6188e5a154c5aef386001e81fe3a4
FrancoVol <franco.volante@polito.it>
2023-09-05 16:47:37 +0200

    Classify_image_custom contains the functions that works with the subscriber

--- subscriber_image.py
+++ subscriber_image.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 
 import json
 import time
-
+import classify_image_custom as image_classify
 
 # Client Name
 CLIENT_NAME = 'IoT'
@@ -15,10 +15,9 @@ def on_connect(client, userdata, flags, rc):  # The callback for when the client
 	client.subscribe('#')
 
 def on_message(client, userdata, message):
-	f = open('receive.jpg', 'wb')
-	f.write(message.payload)
-	f.close()
-	print("image received")
+    print("Message received.")
+    image_classify.mqtt_classify(message.payload)
+	
 
 
 if __name__ == '__main__':
@@ -26,7 +25,7 @@ if __name__ == '__main__':
 	mqttClient = mqtt.Client(CLIENT_NAME)
 	mqttClient.on_connect = on_connect
 	mqttClient.on_message = on_message
-	mqttClient.tls_set(ca_certs="/home/francovolante/python/certs/ca.crt")
+	mqttClient.tls_set(ca_certs="/home/fvolante/ca.crt")
 
 	mqttClient.tls_insecure_set(True)
 	mqttClient.connect(HOST_NAME, port=8883)
