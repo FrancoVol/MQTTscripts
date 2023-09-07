8d51d2529f6eaa31e0baad00015040b9c6fdd61e
FrancoVol <s280136@studenti.polito.it>
2023-09-07 16:40:25 +0200

    Finished the image processing flow

--- images/image_interface.py
+++ images/image_interface.py
@@ -8,15 +8,24 @@ import classify_image_custom as image_classify
 CLIENT_NAME = 'IoT'
 # Broker IP
 HOST_NAME = 'circular.polito.it'
+# Output host IP
+OUTPUT_HOST_NAME = 'test.mosquitto.org'
 
+clients = []
 
 def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
 	print("Connected to broker with result code {0}".format(str(rc)))  # Print result of connection attempt
 	client.subscribe('#')
 
+def on_connect_resp(client, userdata, flags, rc):  # The callback for when the client connects to the broker
+	print("Connected to broker with result code {0}".format(str(rc)))
+
 def on_message(client, userdata, message):
     print("Message received.")
     image_classify.mqtt_classify(message.payload)
+    clients[1].publish("/fvolante/output", "work done")
+    time.sleep(1)
+    print("Done")
     
 	
 
@@ -27,10 +36,17 @@ if __name__ == '__main__':
 	mqttClient.on_connect = on_connect
 	mqttClient.on_message = on_message
 	mqttClient.tls_set(ca_certs="/home/fvolante/ca.crt")
-
 	mqttClient.tls_insecure_set(True)
 	mqttClient.connect(HOST_NAME, port=8883)
-	mqttClient.loop_forever()
+	ret_client =mqtt.Client(CLIENT_NAME)
+	ret_client.on_connect = on_connect_resp
+	ret_client.connect(OUTPUT_HOST_NAME, port=1883)
+	clients.append(mqttClient)
+	clients.append(ret_client)
+	while(True):
+		for client in clients:
+			client.loop(0.1)
+		
 
 	
 	
--- images/image_producer.py
+++ images/image_producer.py
@@ -10,19 +10,21 @@ HOST_NAME = 'circular.polito.it'
 # client name
 CLIENT_NAME = 'test'
 
+
 def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
 	print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
-	
+
+
 if __name__ == '__main__':
 	client =mqtt.Client(CLIENT_NAME)
 	client.on_connect = on_connect
 
-	client.tls_set(ca_certs="/home/francovolante/Desktop/certs/ca.crt")	
+	client.tls_set(ca_certs="/home/franco/Desktop/mqtt/certs/ca.crt")	#absolute path to the certificate
 	client.tls_insecure_set(True)
 	client.connect(HOST_NAME, port=8883)
 	
 	client.loop_start()
-	time.sleep(2)
+	time.sleep(1)
 
 	with open('parrot.jpg', 'rb') as file:
 		file_content = file.read()
@@ -35,3 +37,6 @@ if __name__ == '__main__':
 			print("Error in sending the message")
 	time.sleep(2)
 	client.loop_stop()
+	
+
+	
